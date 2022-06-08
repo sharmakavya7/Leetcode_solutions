b8daeadf10245ba448923b3cee4c40c243b15103
sharmakavya7 <sharmakavya1002@gmail.com>
2022-06-09 00:42:01 +0530

    Runtime: 336 ms, Memory Usage: 16 MB

---
+++ 718. Maximum Length of Repeated Subarray/maximum_Length_of_Repeated_Subarray.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+
+using namespace std;
+class Solution {
+public:
+    int helper(vector<int>& nums1, vector<int>& nums2,int m, int n) {
+        // int t[m+1][n+1];
+        vector<vector<int>> t(m+1,vector<int>(n,-1));
+        for(int i=0; i<m+1; i++) {
+            for(int j=0; j<n+1; j++) {
+                if(i==0 || j==0) {
+                    t[i][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                if(nums1[i-1] == nums2[j-1]){
+                    t[i][j] = 1+t[i-1][j-1];   //(text1, text2, m-1, n-1);
+                } 
+                // else 
+                //     t[i][j] = max(t[i-1][j], t[i][j-1]);
+                else t[i][j]=0;
+            }
+        }
+        // return t[m][n];
+        int mx = INT_MIN;
+        for (int i = 0; i <= m; i++)
+            for (int j = 0; j <= n; j++)
+                mx = max(mx, t[i][j]);
+        return mx;
+    }
+        
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        return helper(nums1, nums2, nums1.size(), nums2.size());
+    }
+};
