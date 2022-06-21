47b541a752917680a6ca9654732ca44f757a0cb4
sharmakavya7 <sharmakavya1002@gmail.com>
2022-06-21 17:59:47 +0530

    Runtime: 98 ms, Memory Usage: 55.5 MB

---
+++ 1004. Max Consecutive Ones III/1004. Max Consecutive Ones III.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+
+using namespace std;
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int mx = 0;
+        int zero=0;
+        int i=0, j=0;
+        while(j<nums.size()) {
+            if(nums[j]==0) {
+                zero++;
+            }
+            while(zero>k) {
+                if(nums[i]==0) {
+                    zero--;
+                }
+                i++;
+            }
+            mx = max(mx,j-i+1);
+            j++;
+            
+        }
+        return mx;
+    }
+};
