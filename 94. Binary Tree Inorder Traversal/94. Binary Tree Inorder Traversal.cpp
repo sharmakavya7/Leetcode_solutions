810aed71e802bfb90eed16bcb944ee07694109b3
sharmakavya7 <sharmakavya1002@gmail.com>
2022-06-23 04:05:19 +0530

    Runtime: 3 ms, Memory Usage: 8.5 MB

---
+++ 94. Binary Tree Inorder Traversal/94. Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+
+//  * Definition for a binary tree node.
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ }; 
+
+class Solution {
+private: 
+    // RECURSIVE
+    void inorder(TreeNode* root, vector<int>&v) {
+        if(!root) {
+            return;
+        }
+        inorder(root->left, v);
+        v.push_back(root->val);
+        inorder(root->right, v);
+    }
+    
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>v;
+        inorder(root, v);
+        return v;   
+    }
+};
