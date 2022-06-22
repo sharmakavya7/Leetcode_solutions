1dc00fe80a49a8ef424045e113fee083f8691149
sharmakavya7 <sharmakavya1002@gmail.com>
2022-06-23 04:21:17 +0530

    Runtime: 0 ms, faster than 100.00% of C++ online submissions, Memory Usage: 8.5 MB,

---
+++ 144. Binary Tree Preorder Traversal/144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+
+//  * Definition for a binary tree node.
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ }; 
+
+class Solution {
+public:
+    void helper(TreeNode* root, vector<int>& v) {
+        if(root == NULL) {
+            return;
+        }
+        v.push_back(root->val);
+        helper(root->left, v);
+        helper(root->right, v);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>v;
+        helper(root, v);
+        return v;
+    }
+};
