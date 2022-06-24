3cf752af26548977ad8302ce2f6bd4fa3ea9e616
sharmakavya7 <sharmakavya1002@gmail.com>
2022-06-24 15:16:30 +0530

    Runtime: 27 ms, Memory Usage: 10.7 MB

---
+++ 559. Maximum Depth of N-ary Tree/559. Maximum Depth of N-ary Tree.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (!root) return 0;
+        int res = 1;
+        for(Node* child : root->children) 
+            res = max(res, maxDepth(child) + 1);
+        return res;
+    }
+};
