576ce11beb1a57464adeedeb148b6c29ac990f68
sharmakavya7 <sharmakavya1002@gmail.com>
2022-06-24 15:19:30 +0530

    Runtime: 8 ms, Memory Usage: 8.2 MB

---
+++ 206. Reverse Linked List/206. Reverse Linked List.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* cur = NULL;
+        while (head) {
+            ListNode* next = head -> next;
+            head -> next = cur;
+            cur = head;
+            head = next;
+        }
+        return cur;
+    }
+};
