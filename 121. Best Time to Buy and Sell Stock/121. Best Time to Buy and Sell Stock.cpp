63856610f72551590eca22fd125d605dd51d5d47
sharmakavya7 <sharmakavya1002@gmail.com>
2022-06-27 04:20:08 +0530

    Runtime: 171 ms, Memory Usage: 93.3 MB

---
+++ 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = INT_MIN;
+        for(int i = 0; i<prices.size(); i++) {
+            minPrice = min(minPrice, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - minPrice);
+        }
+        return maxProfit;
+    }
+};
