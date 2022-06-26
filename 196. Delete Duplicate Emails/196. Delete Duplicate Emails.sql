a0697a8398e16281712407247d916a1583e52349
sharmakavya7 <sharmakavya1002@gmail.com>
2022-06-27 05:05:49 +0530

    SQL, Runtime: 316 ms, Memory Usage: 0B

--- 196. Delete Duplicate Emails/196. Delete Duplicate Emails.sql
+++ 196. Delete Duplicate Emails/196. Delete Duplicate Emails.sql
@@ -1,6 +1,16 @@
 -- # Please write a DELETE statement and DO NOT write a SELECT statement.
+-- Write an SQL query to delete all the duplicate emails, keeping only one unique email with the smallest id
 -- # Write your MySQL query statement below
 DELETE p1
 FROM Person p1, Person p2
 WHERE p1.email = p2.email AND
 p1.id > p2.id
+
+
+-- # Write your MySQL query statement below
+-- Write an SQL query to report all the duplicate emails.
+-- Return the result table in any order.
+
+SELECT email FROM Person
+GROUP BY email 
+HAVING COUNT(id) > 1;
