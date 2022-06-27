634e0da41ea4c2447eb79d5812a0b920d5723448
sharmakavya7 <sharmakavya1002@gmail.com>
2022-06-28 03:12:45 +0530

    Runtime: 0 ms, Memory Usage: 10.1 MB

---
+++ 100. Same Tree/100. Same Tree.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+
+//  * Definition for a binary tree node.
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ }; 
+
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==nullptr || q==nullptr) {
+            return p==q;
+        }
+        return (p->val==q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
