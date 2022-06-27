9446db519dae590348ea3346eb3fe2c5322a7ba0
sharmakavya7 <sharmakavya1002@gmail.com>
2022-06-27 18:21:28 +0530

    Runtime: 35 ms, Memory Usage: 13.6 MB

---
+++ 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    int minPartitions(string n) {
+        int maxi = *max_element(n.begin(),n.end()) - '0';  // LOGIC IS EVERYTHING!!!!
+        return maxi;
+    }
+};
