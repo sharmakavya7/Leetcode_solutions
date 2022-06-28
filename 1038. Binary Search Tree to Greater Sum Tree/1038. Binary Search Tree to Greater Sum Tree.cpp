5600cba3c1a37caed4b55f12e011afbb1d8f6c16
sharmakavya7 <sharmakavya1002@gmail.com>
2022-06-28 22:39:46 +0530

    Runtime: 3 ms, Memory Usage: 8.1 MB

---
+++ 1038. Binary Search Tree to Greater Sum Tree/1038. Binary Search Tree to Greater Sum Tree.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+
+//  * Definition for a binary tree node.
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ }; 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int sum = 0;
+private:
+    void helper(TreeNode* root) {
+        if(!root) {
+            return;
+        }
+        if(root->right) {
+            helper(root->right);
+        }
+        sum+=root->val;
+        root->val = sum;
+        if(root->left) {
+            helper(root->left);
+        }
+    }
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        helper(root);
+        return root;
+    }
+};
