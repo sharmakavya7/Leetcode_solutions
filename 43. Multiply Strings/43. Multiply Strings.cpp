6012f8c544c7aecb30ce8f0220ecaa7f9b8bf287
sharmakavya7 <sharmakavya1002@gmail.com>
2022-07-11 03:23:48 +0530

    Runtime: 7ms, Memory Usage: 6.7 MB

---
+++ 43. Multiply Strings/43. Multiply Strings.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") return "0";
+        
+        vector<int> res(num1.size()+num2.size(), 0);
+        
+        for (int i = num1.size()-1; i >= 0; i--) {
+            for (int j = num2.size()-1; j >= 0; j--) {
+                res[i + j + 1] += (num1[i]-'0') * (num2[j]-'0');
+                res[i + j] += res[i + j + 1] / 10;
+                res[i + j + 1] %= 10;
+            }
+        }
+        
+        int i = 0;
+        string ans = "";
+        while (res[i] == 0) i++;
+        while (i < res.size()) ans += to_string(res[i++]);
+        
+        return ans;
+    }
+};
