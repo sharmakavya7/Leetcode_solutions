e5efe5ffde507f5bee69ab48b8eaf1cdf7c8235d
sharmakavya7 <sharmakavya1002@gmail.com>
2022-07-11 03:25:48 +0530

    Runtime: 0ms, Memory Usage: 10.8 MB

---
+++ 19. Remove Nth Node From End of List/19. Remove Nth Node From End of List.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+
+//  Definition for singly-linked list.
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *fast = head, *slow = head;
+        for (int i = 0; i < n; i++) 
+            fast = fast->next;
+        
+        if (!fast) 
+            return head->next;
+        
+        while (fast->next) {
+            fast = fast->next, slow = slow->next;
+        }
+        slow->next = slow->next->next;
+        
+        return head;
+    }
+    
+};
