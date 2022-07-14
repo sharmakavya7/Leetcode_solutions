0143173c5ffc555bc627bbcbfb65beef4ea6d007
sharmakavya7 <sharmakavya1002@gmail.com>
2022-07-14 21:53:58 +0530

    Runtime: 459ms, Memory Usage: 96.1 MB

---
+++ 1877. Minimize Maximum Pair Sum in Array/1877. Minimize Maximum Pair Sum in Array.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+
+using namespace std;
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=nums.size()-1;
+        int sum = 0;
+        int maxi = INT_MIN;
+        while(i<j) {
+            sum = nums[i++]+nums[j--];
+            maxi = max(sum, maxi);
+        }
+        return maxi;
+    }
+};
