67b6eea21a37a54ae5d1b0c4ff95634ba445a9b6
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-08-17 20:31:23 +0530

    Time: 4 ms (66.17%), Space: 7.7 MB (54.67%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
-    // void helper(int idx, vector<bool>&vis, vector<int>&subs, vector<vector<int>>&ans, vector<int>& nums) {
-    //     if(subs.size()==nums.size()) {
-    //         ans.push_back(subs);
-    //         return;
-    //     }
-    //     vis[idx] = 0;
-    //     for(int i=0; i<nums.size(); i++) {
-    //         if(vis[i]==0) {
-    //             subs.push_back(nums[i]);
-    //             helper(i+1, vis, subs, ans, nums);
-    //             subs.pop_back();
-    //             vis[i]=1;
-    //         }
-    //     }
-    // }
-    void helper(vector<bool>&vis, vector<int>&subs, vector<vector<int>>&ans, vector<int>& nums) {
-        if(subs.size()==nums.size()) {
-            ans.push_back(subs);
+    // by swapping (no need to additional data structure):
+    void helper(int i, vector<vector<int>>&ans, vector<int>& nums) {
+        if(i==nums.size()) {
+            ans.push_back(nums);
             return;
         }
-
-        for(int i=0; i<nums.size(); i++) {
-            if(!vis[i]) {
-                subs.push_back(nums[i]);
-                vis[i]=1;
-                helper(vis, subs, ans, nums);
-                subs.pop_back();
-                vis[i]=0;
+        else {
+            for(int j=i; j<nums.size(); j++) {
+                swap(nums[i],nums[j]);
+                helper(i+1, ans, nums);
+                swap(nums[i],nums[j]);
             }
         }
     }
+//      Another method: by actual backtrack
+//     void helper(vector<bool>&vis, vector<int>&subs, vector<vector<int>>&ans, vector<int>& nums) {
+//         if(subs.size()==nums.size()) {
+//             ans.push_back(subs);
+//             return;
+//         }
+
+//         for(int i=0; i<nums.size(); i++) {
+//             if(!vis[i]) {
+//                 subs.push_back(nums[i]);
+//                 vis[i]=1;
+//                 helper(vis, subs, ans, nums);
+//                 subs.pop_back();
+//                 vis[i]=0;
+//             }
+//         }
+//     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<bool>vis(nums.size(),0);
-        vector<int>subs;
         vector<vector<int>>ans;
-        helper(vis, subs, ans, nums);
+        // vector<bool>vis(nums.size(),0);
+        // vector<int>subs;
+        // helper(vis, subs, ans, nums);
+        
+        // sort(nums.begin(),nums.end());
+        helper(0, ans, nums);
         return ans;
     }
 };
