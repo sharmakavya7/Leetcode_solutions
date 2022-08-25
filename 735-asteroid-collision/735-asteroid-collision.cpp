ef3479f438e84838ecd5d66a24badbd4c7ed8230
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-08-26 01:24:22 +0530

    Time: 27 ms (33.56%), Space: 17.9 MB (12.48%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -2,54 +2,35 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int>ans;
+        stack<int>st;
         for(int i=0; i<asteroids.size(); i++) {
-            if(asteroids[i]>0 || ans.empty() || ans.back()<0){
-                ans.push_back(asteroids[i]);
+            if(asteroids[i]>0 || st.empty()) {
+                st.push(asteroids[i]);
             }
-            else if(ans.back()<=(-asteroids[i])) {
-                if(ans.back()<(-asteroids[i])) {
-                    i--;
+            else if(asteroids[i]<0) {
+                while(!st.empty() && st.top()>0 && st.top()<-asteroids[i]) {
+                    st.pop();
                 }
-                ans.pop_back();
+                if(!st.empty() && st.top()>-asteroids[i]) {
+                    continue;        // -ve one will explode 
+                }
+                
+                else if(!st.empty() && st.top()==-asteroids[i]) {
+                    st.pop();
+                    continue;
+                }
+                else if(st.empty() || st.top()<0) { 
+                    st.push(asteroids[i]);
+                }
             }
         }
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
-        // int neg=0, pos=0;
-        // for(int i=0; i<asteroids.size(); i++) {
-        //     if(asteroids[i]<0) {
-        //         neg = i;
-        //         pos = i-1;
-        //         break;
-        //     }
-        // }
-        // cout<<pos<<" "<<neg<<" ";
-        // while(pos>0 || neg<asteroids.size()) {
-        //     if(asteroids[pos]>(-asteroids[neg])) {
-        //         neg++;
-        //     }
-        //     if(asteroids[pos]<(-asteroids[neg])) {
-        //         pos--;
-        //     }
-        //     if(asteroids[pos]==(-asteroids[neg])) {
-        //         pos--;
-        //         neg++;
-        //     }
-        // }
-        // cout<<pos<<" "<<neg<<" ";
-        // if(pos>=0) {
-        //     for(int i=0; i<=pos; i++) {
-        //         ans.push_back(asteroids[i]);
-        //     }
-        // }
-        // if(neg!=asteroids.size()) {
-        //     for(int i=neg; i<asteroids.size();i++) {
-        //         ans.push_back(asteroids[i]);
-        //     }
-        // }
-        // return ans;
     }
 };
 
-// [1,2,3,4,5,-2,-3,-4,-5,-11,-12,-14]
-// [4,2,-4,-1,90,-2,-4,-5,-11,-12,-14]
-// [-2,-4,-5,6,3,2,7]
+// [8, 9, 6, -7, -9, 12, 50, -34]
