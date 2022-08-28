9ffcec2235af018547d4187ba54f770b7d16d882
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-08-28 14:06:40 +0530

    Time: 30 ms (13.50%), Space: 9.5 MB (16.11%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+//     NAIVE:
+//     TIME : O ( N * LOG N ) -> sort
+//     O ( N ) -> number of diagonals
+//     TOTAL -> O ( N * N * LOG N )
+
+//     SPACE : O ( N ) -> Only on extra vector used
+
+//     Create a vector k in which we will store the elements for a particular diagional.
+//     We simply sort the vector and change the elements of that diagonal with elements of sorted vector.
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        // vector<int>diagonal;
+        map<int, priority_queue<int, vector<int>, greater<int>>> diagonal;
+        
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                // First traverse all diagonals that start from left side.
+                diagonal[j-i].push(mat[i][j]);  // this is getting pushed in a priority queue not in map, map ke [j-i]th key me ye wali priority queue bani hai, jisme hum diagonal elements push kr rhe 
+            }
+        }
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                mat[i][j] = diagonal[j-i].top();
+                diagonal[j-i].pop();
+            }
+        }
+        return mat;
+    }
+};
