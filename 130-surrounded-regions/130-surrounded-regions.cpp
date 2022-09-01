e0f04d020ab4cb70d53a7e4ffd6fa8b9a557897c
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-09-01 19:25:04 +0530

    Time: 30 ms (19.30%), Space: 15.5 MB (9.90%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,58 +1,64 @@
 class Solution {
 public:
-    vector<vector<bool>> solid, visited;
-    int n, m;
-    
-    void dfs(vector<vector<char>>& board, int i, int j){
-        
-        if(i < 0 || j < 0 || i == n || j == m || visited[i][j]==true) 
+    // METHOD-1 (exp in notes)
+    bool seen;
+    void mark(vector<vector<char>>& board,int i,int j,int r,int c)
+    {
+        if(i<0 || i>r-1 || j<0 || j>c-1)
             return;
+        if(board[i][j]=='X')
+            return;
+        
+        board[i][j] = 'X';
+        
+        mark(board,i-1,j,r,c);
+        mark(board,i+1,j,r,c);
+        mark(board,i,j-1,r,c);
+        mark(board,i,j+1,r,c);       
+    }
+    
+    void dfs(vector<vector<char>>& board,int i,int j,int r,int c,vector<vector<bool>>& visited)
+    {
+        if(i<0 || i>r-1 || j<0 || j>c-1)
+            return;
+        
+        if(board[i][j]=='X' || visited[i][j])
+            return;
+        if(i<=0 || i>=r-1 || j<=0 || j>=c-1)
+            seen = true;
         
         visited[i][j] = true;
-        
-        if(board[i][j] == 'X') 
-            return;
-        
-        if(board[i][j] == 'O') 
-            solid[i][j] = true;
-        
-        dfs(board, i + 1, j);
-        dfs(board, i, j + 1);
-        dfs(board, i - 1, j);
-        dfs(board, i, j - 1);
+        // up
+        dfs(board,i-1,j,r,c,visited);
+        // down
+        dfs(board,i+1,j,r,c,visited);
+        // left
+        dfs(board,i,j-1,r,c,visited);
+        // right
+        dfs(board,i,j+1,r,c,visited);
     }
     
     void solve(vector<vector<char>>& board) {
-        n = board.size();
-        m = board[0].size();
+        int r=board.size();
+        if(r<=1)
+            return;
+        int c=board[0].size();
+        if(c<=1)
+            return;
+        vector<vector<bool>> visited(r,vector<bool>(c,false));
         
-        for(int i = 0; i < n; i++){
-            vector<bool> v;
-            for(int j = 0; j < m; j++){
-                v.push_back(false);
-            }
-            solid.push_back(v);
-            visited.push_back(v);
-        }
-        
-        //Step1: Passing cells along the border to find solid cells
-        for(int j = 0; j < m; j++)      
-            dfs(board, 0, j);
-        
-        for(int i = 1; i < n; i++)      
-            dfs(board, i, m - 1);
-        
-        for(int j = m - 2; j >= 0; j--) 
-            dfs(board, n - 1, j);
-        
-        for(int i = n - 2; i >= 1; i--) 
-            dfs(board, i, 0);
-        
-        //Step2: Finding the remaining cells and capturing them
-        for(int i = 1; i < n - 1; i++){
-            for(int j = 1; j < m - 1; j++){
-                if(!solid[i][j] && !visited[i][j] && board[i][j] == 'O') 
-                    board[i][j] = 'X';
+        for(int i=1;i<r-1;++i)
+        {
+            for(int j=1;j<c-1;++j)
+            {
+                if(board[i][j]=='O' && !visited[i][j])
+                {
+                    seen = false;
+                    dfs(board,i,j,r,c,visited);
+                    if(seen==false)
+                        mark(board,i,j,r,c);
+                    seen = true;
+                }
             }
         }
     }
