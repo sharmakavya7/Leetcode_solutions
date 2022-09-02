6e82e314b6fb02050b0021a110fec7ff20d41d06
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-09-02 07:53:32 +0530

    Time: 903 ms (34.70%), Space: 5.9 MB (93.82%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    long long ans = 0;
+    long long result = 0;
     
-    bool isPal(string &s)
-    {
-        int start=0, end = s.length()-1;
-        while(start < end)
-        {
-            if(s[start] != s[end]) return false;
-            start++; end--;
+    bool isPalin(string& s){
+        int i = 0;
+        int j = s.length() - 1;
+ 
+        while (i < j) {
+            if (s[i] != s[j]) return false;
+            i++;
+            j--;
         }
         return true;
     }
@@ -16,33 +17,32 @@ public:
     // include that char in first string
     // include that char in second string
     // exclude that char from both strings
-    void recur(string &s,int idx,string &s1,string &s2)
-    {
-        if(idx == s.size())
-        {
-            if(isPal(s1) and isPal(s2))
-            {
-                long long val =(int) s1.length() * s2.length();
-                ans = max(ans , val);
+    
+    void helper(int i, string& s, string&s1, string&s2) {
+        if(i==s.size()) {
+            if(isPalin(s1)&&isPalin(s2)) {
+                long long val =(int) s1.size() * s2.size();
+                result = max(result, val);
             }
             return;
         }
         // 1. pick s[i] for s1 but don't pick s[i] for s2 (because they should be disjoint)
-        s1.push_back(s[idx]);
-        recur(s,idx+1,s1, s2);
+        s1.push_back(s[i]);
+        helper(i+1, s, s1, s2);
         s1.pop_back();
         
         // 2. pick s[i] for s2 but don't pick s[i] for s1 (because they should be disjoint)
-        s2.push_back(s[idx]);
-        recur(s,idx+1,s1,s2);
+        s2.push_back(s[i]);
+        helper(i+1, s, s1, s2);
         s2.pop_back();
         
         // don't pick s[i] at all - explore this path and find the result then backtrack
-        recur(s,idx+1,s1,s2);
+        helper(i+1, s, s1, s2);
     }
+    
     int maxProduct(string s) {
         string s1="", s2="";
-        recur(s,0,s1,s2);
-        return ans;
+        helper(0, s, s1, s2);
+        return result;
     }
 };
