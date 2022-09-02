ae9f2160b483589957900ff7b2b24b936a8a0c79
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-09-02 18:36:04 +0530

    Time: 9 ms (48.35%), Space: 13.8 MB (24.58%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,23 +11,42 @@
  */
 class Solution {
 public:
-    TreeNode* helper(TreeNode* root, int node) {
-        if(!root) {
-            return new TreeNode(node);
+    // OPTIMAL TC: O(3N)
+    // explanation in notes
+    TreeNode* helper(vector<int>& p, int&i, int upperBound) {
+        if(i==p.size() || p[i]>upperBound) {
+            return NULL;
         }
-        if(node<root->val) {
-            root->left = helper(root->left, node);
-        }
-        else if(node>root->val) {
-            root->right = helper(root->right, node);
-        }
+        TreeNode* root = new TreeNode(p[i++]);
+        root->left = helper(p, i, root->val);
+        root->right = helper(p, i, upperBound);
         return root;
     }
+    
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        TreeNode* root=new TreeNode(preorder[0]);
-        for(int i=1; i<preorder.size(); i++) {
-            helper(root, preorder[i]);
-        }
-        return root;
+        int i=0;
+        return helper(preorder, i, INT_MAX);
     }
-};
+    
+    
+    // BRUTE TC: O(N*N)
+    // TreeNode* helper(TreeNode* root, int node) {
+    //     if(!root) {
+    //         return new TreeNode(node);
+    //     }
+    //     if(node<root->val) {
+    //         root->left = helper(root->left, node);
+    //     }
+    //     else if(node>root->val) {
+    //         root->right = helper(root->right, node);
+    //     }
+    //     return root;
+    // }
+    // TreeNode* bstFromPreorder(vector<int>& preorder) {
+    //     TreeNode* root=new TreeNode(preorder[0]);
+    //     for(int i=1; i<preorder.size(); i++) {
+    //         helper(root, preorder[i]);
+    //     }
+    //     return root;
+    // }
+}; 
