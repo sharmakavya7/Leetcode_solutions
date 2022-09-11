5f99919cfabf1825819a3967bc138edfeed71592
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-09-11 11:38:58 +0530

    Time: 52 ms (11.42%), Space: 18.6 MB (5.99%) - LeetHub

---
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        priority_queue<int>pq;
+        
+        for(auto i:nums) {
+            pq.push(i);
+        }
+        for(int i=1; i<nums.size(); i+=2) {
+            nums[i] = pq.top();
+            pq.pop();
+        }
+        int i = 0;
+        for(int i=0; i<nums.size(); i+=2) {
+            nums[i] = pq.top();
+            pq.pop();
+        }
+    }
+};
