2163821c077ed137c6b68d4357c67d8ca267fa93
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-09-11 11:59:24 +0530

    Time: 617 ms (32.07%), Space: 69.5 MB (57.10%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    static bool cmp(string &a, string &b){
-        if(a.size() == b.size())
-            return a<b;  //lexicographical comparision
-        else
-            return a.size() < b.size();
-    }
+    struct comparator {
+        bool operator () (string&a, string&b) {
+            if(a.size()==b.size()) {
+                return a>b;
+            }
+            else {
+                return a.size()>b.size();
+            }
+        }
+    };
     string kthLargestNumber(vector<string>& nums, int k) {
-        sort(nums.begin(), nums.end(), cmp);
-        return nums[nums.size()-k];
+        priority_queue<string, vector<string>, comparator>pq;
         
-        // priority_queue<int, vector<int>, greater<int>>pq;
-        // for(int i=0; i<nums.size(); i++) {
-        //     int num = stoi(nums[i]);
-        //     pq.push(num);
-        //     if(pq.size()>k) {
-        //         pq.pop();
-        //     }
-        // }
-        // string ans = to_string(pq.top());
-        // return ans;
+        for(int i=0; i<nums.size(); i++) {
+            pq.push(nums[i]);
+            if(pq.size()>k) {
+                pq.pop();
+            }
+        }
+        
+        return pq.top();
     }
 };
+
+// BY COMPARATOR:
+
+// static bool cmp(string &a, string &b){
+//         if(a.size() == b.size())
+//             return a<b;  //lexicographical comparision
+//         else
+//             return a.size() < b.size();
+//     }
+//     string kthLargestNumber(vector<string>& nums, int k) {
+//         sort(nums.begin(), nums.end(), cmp);
+//         return nums[nums.size()-k];
+        
+        
