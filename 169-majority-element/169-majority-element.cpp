4d5030e0f26c9e376929730fed9dacccdf1adac9
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-09-14 01:49:08 +0530

    Time: 54 ms (5.64%), Space: 19.5 MB (59.91%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         //boyer moore voting algo
-        int ansIndex = 0;
-        int count = 0;
-        for(int i = 0; i<nums.size(); i++) {
-            if(nums[i] == nums[ansIndex]) {
-                count++;
-            }
-            else count--;
-            if(count == 0) {
-                ansIndex = i;
-                count = 1;
-            }
-        }
-        return nums[ansIndex];
+        // int ansIndex = 0;
+        // int count = 0;
+        // for(int i = 0; i<nums.size(); i++) {
+        //     if(nums[i] == nums[ansIndex]) {
+        //         count++;
+        //     }
+        //     else count--;
+        //     if(count == 0) {
+        //         ansIndex = i;
+        //         count = 1;
+        //     }
+        // }
+        // return nums[ansIndex];
+        
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
     }
 };
 
