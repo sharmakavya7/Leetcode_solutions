79cc9620b61ffea531e8800857ce8b5ccb2d6368
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-09-13 16:35:53 +0530

    Time: 18 ms (23.52%), Space: 13.4 MB (25.82%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>>vis(m, vector<int>(n,0));
+        queue<pair<pair<int,int>,int>>q;
+        
+        // traverse and get all rotten oranges
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j]==2) {
+                    q.push({{i,j},0});
+                    vis[i][j]=2;
+                }
+                // else vis[i][j]=0;
+            }
+        }
+        int delrow[] = {-1, 0, +1, 0};
+        int delcol[] = {0, +1, 0, -1};
+        // up, right, down, left
+        int time = 0;
+        
+        while(!q.empty()) {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int t = q.front().second;
+            q.pop();
+            time = max(time, t);
+            // visit all the neighbouring guys
+            for(int i=0; i<4; i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n 
+                    && grid[nrow][ncol]==1 && vis[nrow][ncol]!=2) {
+                        q.push({{nrow,ncol},t + 1});
+                        vis[nrow][ncol]=2;
+                }
+            }
+        }
+        // bool flag = false;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(vis[i][j]!=2 && grid[i][j]==1) {
+                    return -1;
+                }
+            }
+        }
+        return time;
+    }
+};
