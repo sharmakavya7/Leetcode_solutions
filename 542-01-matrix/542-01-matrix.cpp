7eb35ccfc373bc21a07b60bd0cfab21d1fb67471
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-09-14 16:52:58 +0530

    Time: 158 ms (34.85%), Space: 30.3 MB (56.52%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int x[4] = {-1, 0, +1, 0};
+    int y[4] = {0, +1, 0, -1};
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m=mat.size();
+        if(m==0) return {};
+        int n=mat[0].size();
+        vector<vector<int> > dis(m,vector<int>(n,-1));
+        queue<pair<int,int> > que;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    que.push({i,j});
+                    dis[i][j]=0;
+                }
+            }
+        }
+        while(!que.empty()){
+            pair<int,int> point=que.front();
+            que.pop();
+            for(int dir=0;dir<4;dir++){
+                int xd=point.first+x[dir];
+                int yd=point.second+y[dir];
+                if(xd>=0 && yd>=0 && xd<=m-1 && yd<=n-1 
+                   && mat[xd][yd]==1 && dis[xd][yd]==-1){
+                    dis[xd][yd]=dis[point.first][point.second]+1;
+                    que.push({xd,yd});
+                }
+            }
+        }
+        return dis;
+    }
+};
