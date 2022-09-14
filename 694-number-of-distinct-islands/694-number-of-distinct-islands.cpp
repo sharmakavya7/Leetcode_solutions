09c8ae67f33e943f8bde8973057e507f2c5a68a6
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-09-14 20:14:49 +0530

    Time: 83 ms (39.49%), Space: 30.1 MB (44.71%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.cpp
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // SC: O(m x n)
+    // T.C: O(m x n) * log(m x n) + O(m x n x 4)
     int DR[4] = {-1, 0, +1, 0};
     int DC[4] = {0, +1, 0, -1};
     
@@ -23,13 +25,14 @@ public:
         int n = grid[0].size();
         
         vector<vector<int>>vis(m, vector<int>(n,0));
-        set<vector<pair<int,int>>>st;
+        set<vector<pair<int,int>>>st;   // space: m x n
+        // tc: O(m x n)
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(!vis[i][j]&&grid[i][j]==1) {
                     vector<pair<int,int>>temp;
-                    dfs(i, j, vis, grid, temp, i, j);
-                    st.insert(temp);
+                    dfs(i, j, vis, grid, temp, i, j);  // tc: m x n x 4 (4 directions)
+                    st.insert(temp);  // tc: log(st.length) -> log(m x n)
                 //     vis[i][j]=1;
                 }
             }
