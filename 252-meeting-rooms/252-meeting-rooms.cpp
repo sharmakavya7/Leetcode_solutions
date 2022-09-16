d223686e6091245993c5a9079e452953127af49e
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-09-16 22:35:54 +0530

    Time: 42 ms (20.26%), Space: 11.7 MB (77.49%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.cpp
+++ 252-meeting-rooms/252-meeting-rooms.cpp
@@ -8,21 +8,17 @@ public:
             return true;
         }
         sort(intervals.begin(),intervals.end());
-        bool ans = true;
+
         pair<int,int>meets(intervals[0][0], intervals[0][1]);
-        // meets.push_back(intervals[0]);
         for(int i=1; i<intervals.size(); i++) {
             if(intervals[i][0] >= meets.second) {
-                // meets[1] = max(meets[1],intervals[i][1]);
-                // meet = intervals[i];
                 meets.first = intervals[i][0];
                 meets.second = intervals[i][1];
-                ans = true;
             }
             else {
                 return false;
             }
         }
-        return ans;
+        return true;
     }
 };
