219a2cd88fe952722d950ddb33bdc32acdc6dcd5
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-09-22 12:54:26 +0530

    Time: 36 ms (25.85%), Space: 9.6 MB (90.16%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string ans;
+        // inplace
+        int i=0; 
+        for(int j=0; j<s.size(); j++) {
+            if(s[j]==' ') {
+                reverse(s.begin()+i, s.begin()+j);
+                i = j+1;
+            }
+        }
+        // for the last word
+        reverse(s.begin() + i, s.end());
+        return s;
+    }
+};
+
+// BRUTE:
+/*
+string ans;
         string temp = "";
         for(int i=0; i<s.size(); i++) {
             // string temp = "";
@@ -21,5 +37,5 @@ public:
         reverse(temp.begin(),temp.end());
         ans+=temp;
         return ans;
-    }
-};
+
+*/
