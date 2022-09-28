42d90003fe5a048a23774c44d5aeb9b9624e8f96
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-09-28 22:46:37 +0530

    Time: 169 ms (35.60%), Space: 30.3 MB (58.86%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -4,26 +4,37 @@ public:
     int y[4] = {0, +1, 0, -1};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m=mat.size();
-        if(m==0) return {};
+        if(m==0) 
+            return {};
+        
         int n=mat[0].size();
         vector<vector<int> > dis(m,vector<int>(n,-1));
         queue<pair<int,int> > que;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(mat[i][j]==0){
+        
+        for(int i=0;i<m;i++) {
+            
+            for(int j=0;j<n;j++) {
+                
+                if(mat[i][j]==0) {
+                    
                     que.push({i,j});
                     dis[i][j]=0;
                 }
             }
         }
-        while(!que.empty()){
+        while(!que.empty()) {
+            
             pair<int,int> point=que.front();
             que.pop();
-            for(int dir=0;dir<4;dir++){
-                int xd=point.first+x[dir];
-                int yd=point.second+y[dir];
+            
+            for(int k=0;k<4;k++) {
+                
+                int xd=point.first+x[k];
+                int yd=point.second+y[k];
+                
                 if(xd>=0 && yd>=0 && xd<=m-1 && yd<=n-1 
                    && mat[xd][yd]==1 && dis[xd][yd]==-1){
+                    
                     dis[xd][yd]=dis[point.first][point.second]+1;
                     que.push({xd,yd});
                 }
