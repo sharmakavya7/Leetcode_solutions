2ea525226fc85b30099995638e21acaa629f9541
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-10-12 03:56:54 +0530

    Time: 3 ms (56.07%), Space: 7.7 MB (77.60%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -19,23 +19,35 @@ public:
 //         return dp[i] = max(steal, notSteal);
 //     }
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(), 0);
-        // int ans = maxMoney(nums.size()-1, nums, dp);
+        // space optimization
+        int prev = nums[0], prev2 = 0, curi = 0;
         int ans = 0;
-        int n = nums.size();
-        dp[0] = nums[0];
-        // dp[1] = 
-        
-        for(int i=1; i<nums.size(); i++) {
-            int steal = nums[i];
-            if(i>=2) {
-                steal += dp[i-2];
-            }
-            int noSteal = 0 + dp[i-1];
-            
-            dp[i] = max(steal, noSteal);
+        for(int i = 1; i<nums.size(); i++) {
+            int steal = nums[i] + prev2;
+            int noSteal = 0 + prev;
+            curi = max(steal, noSteal);
+            prev2 = prev;
+            prev = curi;
         }
-        
-        return dp[n-1];
+        return prev;
     }
 };
+
+// DP tabulation:
+// vector<int>dp(nums.size(), 0);
+//         // int ans = maxMoney(nums.size()-1, nums, dp);
+//         int ans = 0;
+//         int n = nums.size();
+//         dp[0] = nums[0];
+        
+//         for(int i=1; i<nums.size(); i++) {
+//             int steal = nums[i];
+//             if(i>=2) {
+//                 steal += dp[i-2];
+//             }
+//             int noSteal = 0 + dp[i-1];
+            
+//             dp[i] = max(steal, noSteal);
+//         }
+        
+//         return dp[n-1];
