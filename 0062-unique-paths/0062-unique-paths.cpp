ffde9f4e916fe99bb0df26bd049c19055c6a091a
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-10-31 17:55:06 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (50.56%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    const int N = 101;
-    int dp[101][101];
-    int solve(int i, int j) {
-        if(i == 0 && j == 0) {
-            return 1;
+    // T.C. => m x n
+    // S.C. => m x n
+
+    int solve(int m, int n, vector<vector<int>>& dp) {
+        // dp[0][0] = 1;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int up = 0, left = 0;
+                if(i > 0)
+                    up = dp[i-1][j];
+                if(j > 0)
+                    left = dp[i][j-1];
+                
+                dp[i][j] = up + left;
+            }
         }
-        if(i < 0 || j < 0) {
-            return 0;
-        }
-        if(dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        int up = solve(i - 1, j);
-        int left = solve(i, j - 1);
-        
-        return dp[i][j] = up + left;
+        return dp[m-1][n-1];
     }
     int uniquePaths(int m, int n) {
-        memset(dp, -1, sizeof(dp));
-        return solve(m-1, n-1);
+        vector<vector<int> > dp(m,vector<int>(n,-1));
+        // memset(dp, 0, sizeof(dp));
+        return solve(m, n, dp);
     }
 };
