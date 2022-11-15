43d0cae908d7d0eebaeb296461d09ca3d374a2b4
Kavya Sharma <85942381+sharmakavya7@users.noreply.github.com>
2022-11-15 16:46:31 +0530

    Time: 5 ms (33.46%), Space: 6.3 MB (26.81%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
-    
-    bool isValid(char a,char b){
-        return a == '1'||(a == '2' && b <='6');
-    }
-    
-    bool isValid(char a){
-        return a != '0';
-    }
-    
-    int numDecodings(string s) {
-        
-        int n = s.size();
-        if ( n == 0 || s[0] == '0' ) return 0;
-        if ( n == 1 ) return 1;
-        int prev_ways = 1;   // upto index i-1
-        int cur_ways = 1;          // upto index i (cur index)
-        int num;             // temp to store i ways till cur index
-
-        for ( int i = 1; i < n; i++ ) {
-
-            num = 0;
-            if ( !isValid(s[i]) && !isValid(s[i-1], s[i]) ) 
-                return 0;
-
-            if ( isValid(s[i]) )
-                num = prev_ways;
-
-            if ( isValid(s[i-1], s[i]) ) 
-                num += cur_ways;
-
-            cur_ways = prev_ways;
-            prev_ways = num;
+    vector<int>dp;
+    int decode(string &s, int i) {
+        if(i == s.size()) {
+            return 1;
         }
-        return num;
+        if(dp[i] != -1) {
+            return dp[i];
+        }
+        int ans = 0;
+        // 2 cases, 1st: s[i] != '0', 2nd: s[i] == '1' || s[i] == '2' && s[i+1] <= '6'
+        if(s[i] != '0') {
+            ans += decode(s, i+1);
+        }
+        if(i+1 < s.size() && ( s[i] == '1' || s[i] == '2' && s[i+1] <= '6')) {
+            ans += decode(s, i+2);
+        }
+        return dp[i] = ans;
     }
-    
+    int numDecodings(string s) {
+        dp.resize(101, -1);
+        return decode(s, 0);
+    }
 };
